b464e14b0219a749b0d480cb8bd6670781c44a2c
hossix <josv525@hotmail.com>
2023-09-25 23:08:11 -0400

    Complete P1

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -7,8 +7,11 @@ function processArray($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
-    //TODO add logic here to echo out only odd values
-
+    for ($i=0; $i<count($arr);$i+=1){
+        if ( $arr[$i] % 2 == 1){    
+            echo "$arr[$i]";
+        }
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
