4dc440980f7bb0ae17071ac745e98a9ff57caae8
hossix <josv525@hotmail.com>
2023-12-09 20:27:09 -0500

    API-Intro

--- partials/nav.php
+++ partials/nav.php
@@ -28,8 +28,8 @@ session_start();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 <!-- include css and js files -->
-<link rel="stylesheet" href="<?php echo get_url('/public_html/Project/styles.css'); ?>">
-<script src="<?php echo get_url('/public_html/Project/helpers.js'); ?>"></script>
+<link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
+<script src="<?php echo get_url('helpers.js'); ?>"></script>
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
