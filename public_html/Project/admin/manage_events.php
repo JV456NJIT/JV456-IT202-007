e19e87dde0d00c944f4200636a4c741c113e9eba
hossix <josv525@hotmail.com>
2023-12-10 21:32:38 -0500

    Final API to DB changes

--- public_html/Project/admin/manage_events.php
+++ public_html/Project/admin/manage_events.php
@@ -6,158 +6,59 @@ if (!has_role("Admin")) {
     flash("You don't have permission to view this page", "warning");
     die(header("Location: " . get_url("home.php")));
 }
-//TODO need to update insert_breeds... to use the $mappings array and not go based on is_int for value
-function insert_breeds_into_db($db, $events, $mappings)
-{
-    // Prepare SQL query
-    $query = "INSERT INTO `events` ";
-    if (count($events) > 0) {
-        $cols = array_keys($events[0]);
-        $query .= "(" . implode(",", array_map(function ($col) {
-            return "`$col`";
-        }, $cols)) . ") VALUES ";
 
-        // Generate the VALUES clause for each breed
-        $values = [];
-        foreach ($events as $i => $description) {
-            $descriptionPlaceholders = array_map(function ($v) use ($i) {
-                return ":" . $v . $i;  // Append the index to make each placeholder unique
-            }, $cols);
-            $values[] = "(" . implode(",", $descriptionPlaceholders) . ")";
-        }
-
-        $query .= implode(",", $values);
-
-        // Generate the ON DUPLICATE KEY UPDATE clause
-        $updates = array_reduce($cols, function ($carry, $col) {
-            $carry[] = "`$col` = VALUES(`$col`)";
-            return $carry;
-        }, []);
-
-        $query .= " ON DUPLICATE KEY UPDATE " . implode(",", $updates);
-
-        // Prepare the statement
-        $stmt = $db->prepare($query);
-
-        // Bind the parameters for each breed
-        foreach ($events as $i => $description) {
-            foreach ($cols as $col) {
-                $placeholder = ":$col$i";
-                $val = isset($breed[$col]) ? $description[$col] : "";
-                $param = PDO::PARAM_STR;
-                if (str_contains($mappings[$col], "int")) {
-                    $param = PDO::PARAM_INT;
-                }
-                $stmt->bindValue($placeholder, $val, $param);
-            }
-        }
-
-        // Execute the statement
-        try {
-            $stmt->execute();
-        } catch (PDOException $e) {
-            error_log(var_export($e, true));
-        }
-    }
-}
-
-function process_single_breed($breed, $columns, $mappings)
-{
-    // Process breed data
-    $weight = isset($breed["weight"]) ? se($breed["weight"], "imperial", "0-0", false) : "0-0";
-    $life_span = se($breed, "life_span", "12-15", false);
-    $weight = array_map('trim', explode('-', $weight));
-    $life_span = array_map('trim', explode('-', $life_span));
-
-    // Prepare record
-    $record = [];
-    $record["api_id"] = se($breed, "id", "", false);
-    $record["min_weight_lbs"] = $weight[0];
-    $record["max_weight_lbs"] = $weight[1];
-    $record["min_life_span_years"] = $life_span[0];
-    $record["max_life_span_years"] = $life_span[1];
-    $record["urls"] = implode(",", array_intersect_key($breed, array_flip(["wikipedia_url", "vcahospitals_url", "vetstreet_url", "cfa_url"])));
-
-    // Map breed data to columns
-    foreach ($columns as $column) {
-        if(in_array($columns, ["id", "api_id", "urls"])){
-            continue;
-        }
-        if(array_key_exists($column, $breed)){
-            $record[$column] = $breed[$column];
-            if(empty($record[$column])){
-                if(str_contains($mappings[$column], "int")){
-                    $record[$column] = "0";
-                }
-            }
-        }
-    }
-    error_log("Record: " . var_export($record, true));
-    return $record;
-}
-
-function process_breeds($result)
-{
+function process_events($result){
     $status = se($result, "status", 400, false);
     if ($status != 200) {
         return;
     }
-
-    // Extract data from result
-    $data_string = html_entity_decode(se($result, "response", "{}", false));
-    $wrapper = "{\"data\":$data_string}";
-    $data = json_decode($wrapper, true);
-    if (!isset($data["data"])) {
-        return;
+    error_log("Response: " . var_export($result, true));
+    if (se($result, "status", 400, false) == 200 && isset($result["response"])) {
+        $result = json_decode($result["response"], true);
+    } else {
+        $result = [];
     }
-    $data = $data["data"];
-    error_log("data: " . var_export($data, true));
-    // Get columns from CA_Breeds table
     $db = getDB();
-    $stmt = $db->prepare("SHOW COLUMNS FROM CA_Breeds");
-    $stmt->execute();
-    $columnsData = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    // Prepare columns and mappings
-    $columns = array_column($columnsData, 'Field');
-    $mappings = [];
-    foreach ($columnsData as $column) {
-        $mappings[$column['Field']] = $column['Type'];
-    }
-    $ignored = ["id", "created", "modified"];
-    $columns = array_diff($columns, $ignored);
-
-    // Process each breed
-    $breeds = [];
-    foreach ($data as $breed) {
-        $record = process_single_breed($breed, $columns, $mappings);
-        array_push($breeds, $record);
-    }
-
-    // Insert breeds into database
-    insert_breeds_into_db($db, $breeds, $mappings);
-}
-
-$action = se($_POST, "action", "", false);
-if ($action) {
-    switch ($action) {
-        case "breeds":
-            $result = get("https://api.thecatapi.com/v1/breeds", "CAT_API_KEY", ["limit" => 75, "page" => 0], false);
-            process_breeds($result);
-            break;
+    $i = 0;
+    foreach ($result as $element){
+        foreach ($element as $event){
+            $descriptionData = $event["description"];
+            $imageData = $event["image"];
+            $artistData = $event["name"];
+            $locationData = $event["location"]["name"];
+            $timeData = $event["startDate"];
+            $api_idData = $artistData . "P:" . $result["page"] . "E:" .$i;
+            echo $descriptionData,"<br>",$artistData, "<br>", " at the ", $locationData, "<br>" ," on ", date('D-M-d-Y h:i', strtotime($timeData));
+            $timeData = date('Y-m-d H:i:s', strtotime($timeData));
+            $query = "INSERT INTO Events (api_id, description, image, artist , location, time) VALUES(:api_id, :description, :image, :artist, :location, :time) ON DUPLICATE KEY UPDATE api_id=api_id";
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(':api_id',$api_idData,PDO::PARAM_STR);
+            $stmt->bindValue(':description',$descriptionData,PDO::PARAM_STR);
+            $stmt->bindValue(':image',$imageData,PDO::PARAM_STR);
+            $stmt->bindValue(':artist',$artistData,PDO::PARAM_STR);
+            $stmt->bindValue(':location',$locationData,PDO::PARAM_STR);
+            $stmt->bindValue(':time',$timeData,PDO::PARAM_STR);
+            $stmt->execute();
+            $i++;
+        }
     }
 }
 ?>
 
 <div class="container-fluid">
-    <h1>Cat Data Management</h1>
-    <div class="row">
-        <div class="col">
-            <!-- Breed refresh button -->
-            <form method="POST">
-                <input type="hidden" name="action" value="breeds" />
-                <input type="submit" class="btn btn-primary" value="Refresh Breeds" />
-            </form>
-        </div>
+    <h1>Artist Event Manager</h1>
+    <div class="container-fluid">
+        <form  method="POST">
+            <?php render_input(["type" => "text", "id" => "artistName", "name" => "artistName", "label" => "Artist Name", "rules" => ["required" => true]]); ?>
+            <?php render_input(["type" => "number", "id" => "pageOption", "name" => "pageOption", "label" => "Page Option", "rules" => ["required" => true]]); ?>
+            <?php render_button(["text" => "Load Artist Events", "type" => "submit"]); ?>
+        </form>
     </div>
 </div>
+
+<?php if (isset($_POST['artistName'])) : ?>
+    <?php
+        $result = get("https://concerts-artists-events-tracker.p.rapidapi.com/artist", "EVENT_API_KEY", ["name" => $_POST['artistName'], "page" => $_POST['pageOption']], true, "concerts-artists-events-tracker.p.rapidapi.com");
+        process_events($result);
+    ?>
+<?php endif ?>
--- public_html/Project/sql/006_create_table_events.sql
+++ public_html/Project/sql/006_create_table_events.sql
@@ -1,12 +1,12 @@
-CREATE TABLE events(
-    'id'    int auto_increment_not null,
-    'api_id'    VARCHAR(10),
-    'description'   TEXT,
-    'image', TEXT
-    'artist', TEXT,
-    'location', TEXT,
-    'time',TIMESTAMP,
-    PRIMARY KEY ('id'),
-    UNIQUE KEY ('api_id'),
-    UNIQUE KEY ('description')
+CREATE TABLE IF NOT EXISTS `Events`(
+    `id` INT AUTO_INCREMENT,
+    `api_id` VARCHAR(255),
+    `description` VARCHAR(255),
+    `image` TEXT,
+    `artist` TEXT,
+    `location` TEXT,
+    `time` TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`api_id`),
+    UNIQUE KEY (`description`)
 )
