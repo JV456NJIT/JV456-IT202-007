b147862f22ea03bfd791d728ab4631937b49e828
hossix <josv525@hotmail.com>
2023-12-12 14:35:36 -0500

    save data echo removal

--- lib/save_data.php
+++ lib/save_data.php
@@ -13,7 +13,6 @@ function save_data($data, $ignore = ["submit"]){
     $api_idData = $artistData . ":MANUAL";
     $time = time();
     $modified = date('Y-m-d H:i:s', $time);
-    echo $modified;
     $query = "INSERT INTO Events (api_id, description, image, artist , venue, country, locality, time, modified) VALUES(:api_id, :description, :image, :artist, :venue, :country, :locality, :time, :modified) ON DUPLICATE KEY UPDATE api_id=api_id";
     $stmt = $db->prepare($query);
     $stmt->bindValue(':api_id',$api_idData,PDO::PARAM_STR);
